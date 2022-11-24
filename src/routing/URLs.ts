8cdb5eac185a43953131eb0e2bf7618497a76180
Khrulenko Vitalii <khrulenko.v@gmail.com>
2022-11-24 19:36:22 +0200

    fixed urls

--- src/routing/URLs.ts
+++ src/routing/URLs.ts
@@ -1,16 +1,12 @@
 import { createUrl } from '../common/utils';
 
-const APP_NAME: string = '/thebudget';
-
 //home page
-const HOME: string = 'home';
-export const URL_HOME: string = createUrl(APP_NAME, HOME);
+export const URL_HOME: string = '/home';
 
 //auth
-const AUTH: string = 'auth';
+export const URL_AUTH: string = '/auth';
 const LOGIN: string = 'login';
 const REGISTRATION: string = 'registration';
 
-export const URL_AUTH: string = createUrl(APP_NAME, AUTH);
 export const URL_AUTH_LOGIN: string = createUrl(URL_AUTH, LOGIN);
 export const URL_AUTH_REGISTRATION: string = createUrl(URL_AUTH, REGISTRATION);
