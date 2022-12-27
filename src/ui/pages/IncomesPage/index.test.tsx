314d11fbb66d8e1795e1755652904aad7ea7f789
Khrulenko Vitalii <khrulenko.v@gmail.com>
2022-12-27 20:06:56 +0200

    added unit tests

--- src/ui/pages/IncomesPage/index.test.tsx
+++ src/ui/pages/IncomesPage/index.test.tsx
@@ -1,19 +1,72 @@
 import '@testing-library/jest-dom';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import IncomesPage from '.';
-import { mockedUser, mockedEmptyIncomes } from '../../../common/mocks';
+import {
+  mockedUser,
+  mockedEmptyIncomes,
+  mockedIncomes,
+} from '../../../common/mocks';
 import { renderWithRedux } from '../../../common/testUtils';
 
-const emptyState = {
+const emptyIncomesState = {
   user: mockedUser,
   incomes: mockedEmptyIncomes,
 };
 
-describe('IncomesPage', () => {
-  test('should show "no incomes" allert if there are no incomes', () => {
-    renderWithRedux(<IncomesPage />, { initialState: emptyState });
-    const noIncomesAllert = screen.getByText(/there are no incomes/i);
+const loadedIncomesState = {
+  user: mockedUser,
+  incomes: mockedIncomes,
+};
 
-    expect(noIncomesAllert).toBeInTheDocument();
+describe('IncomesPage', () => {
+  test('shows "create income" modal after click on "add income" button', () => {
+    renderWithRedux(<IncomesPage />, { initialState: emptyIncomesState });
+
+    const addIncomeButton = screen.getByTestId('addIncomeButton');
+
+    userEvent.click(addIncomeButton);
+
+    const createIncomeModal = screen.getByText(/create new income/i);
+    expect(createIncomeModal).toBeInTheDocument();
+  });
+
+  describe('if there are no incomes', () => {
+    test('shows "no incomes" allert ', () => {
+      renderWithRedux(<IncomesPage />, { initialState: emptyIncomesState });
+      const noIncomesAllert = screen.getByText(/there are no incomes/i);
+
+      expect(noIncomesAllert).toBeInTheDocument();
+    });
+  });
+
+  describe('if there are incomes', () => {
+    beforeEach(() =>
+      renderWithRedux(<IncomesPage />, {
+        initialState: loadedIncomesState,
+      })
+    );
+
+    test('shows "delete income" modal after click on "delete income" button', () => {
+      const [firstDelIncButton] = screen.getAllByTestId('deleteIncomeButton');
+
+      userEvent.click(firstDelIncButton);
+
+      const deleteIncomeModal = screen.getByText(
+        /are you sure you want to delete this income/i
+      );
+
+      expect(deleteIncomeModal).toBeInTheDocument();
+    });
+
+    test('shows "change income" modal after click on "edit income" button', () => {
+      const [firstEditIncButton] = screen.getAllByTestId('editIncomeButton');
+
+      userEvent.click(firstEditIncButton);
+
+      const changeIncomeModal = screen.getByText(/change income/i);
+
+      expect(changeIncomeModal).toBeInTheDocument();
+    });
   });
 });
